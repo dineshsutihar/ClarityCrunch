9905aa34fc987f39d3efc86118c46af1e8465e14
Dinesh Kumar Sutihar <dineshsutihar9@gmail.com>
2024-07-01 22:25:52 +0530

    feat: completes full backend

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
+require("dotenv").config();
+const summarizeText = require("./summarize.js");
 
+const port = process.env.PORT;
 // Parses JSON bodies (as sent by API clients)
 app.use(express.json());
 
 // Serves static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// summarize endpoint
+app.post("/summarize", (req, res) => {
+  const text = req.body.text_to_summarize;
+
+  summarizeText(text)
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+});
 
 // Start the server
 app.listen(port, () => {
